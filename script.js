9db258ddf5f705ceb89bb0ed72b747a009571dbd
Eva van Rooden <evavanrooden@gmail.com>
2019-01-08 21:45:14 +0100

    setup DOM

--- script.js
+++ script.js
@@ -4,22 +4,74 @@
 
 console.log("Welcome to blackjack!"); // my first JS code
 
+// changing html
+
+let textArea = document.getElementById('text-area');
+let newGameButton = document.getElementById('new-game-button');
+let hitButton = document.getElementById('hit-button');
+let stayButton = document.getElementById('stay-button');
+
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+newGameButton.addEventListener('click', function() {
+  textArea.innerText = 'New game!';
+  newGameButton.style.display = 'none';
+  hitButton.style.display = 'inline';
+  stayButton.style.display = 'inline';
+});
+
+let okButton = document.getElementById('ok-button');
+okButton.addEventListener('click', function() {
+  paragraph.innerText = 'Button clicked!';
+});
+
 // creating the deck of cards
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let values = ["Ace", "Two", "Three", "Four", "Five", "Six",
               "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
-let deck = [];
+let cards = [
+    {
+      suit: "Hearts",
+      value: "Queen"
+    }
+];
 
-for(let i = 0; i < suits.length; i++) {
-  for(let j = 0; j < values.length; j++) {
-    deck.push(values[j] + " of " + suits[i]);
+let result = Math.random() * 52;
+
+result = Math.trunc(result);
+
+console.log("Random: " + result);
+
+
+console.log(cards);
+
+function createDeck() {
+  let deck = [];
+  for(let i = 0; i < suits.length; i++) {
+    for(let j = 0; j < values.length; j++) {
+      let card = {
+        suit: suits[i],
+        value: values[j]
+      };
+      deck.push(card);
+    }
   }
-
+  return deck;
 }
+
+function getNextCard() {
+  return deck.shift();
+}
+
+function getCardString(card) {
+  return card.value + " of " + card.suit;
+}
+
+let deck = createDeck();
 console.log(deck);
 
-let playerCards = [deck[0], deck[2]];
+let playerCards = [getNextCard(), getNextCard()];
 
 console.log("You are dealt:");
-console.log("  " + playerCards[0]);
-console.log("  " + playerCards[1]);
+console.log(getCardString(playerCards[0]));
+console.log("  " + getCardString(playerCards[1]));
